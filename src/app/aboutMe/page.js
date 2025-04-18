ef2b96b82be9c876e5c785012f54704cd589988e
zichen <zzcnhy@gmail.com>
2025-04-18 16:15:35 +0800

    basic structure for personal page

---
+++ src/app/aboutMe/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Sidebar from "./components/Sidebar";
+
+export default function AboutMe() {
+  return (
+    <div className="min-h-screen bg-primary">
+      <Sidebar />
+      <main className="p-8">
+        <h1 className="text-4xl font-bold">About Me</h1>
+        {/* Add your about me content here */}
+      </main>
+    </div>
+  );
+}
---
+++ src/app/experience/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Sidebar from "./components/Sidebar";
+
+export default function Experience() {
+  return (
+    <div className="min-h-screen bg-primary">
+      <Sidebar />
+      <main className="p-8">
+        <h1 className="text-4xl font-bold">Experience</h1>
+        {/* Add your experience content here */}
+      </main>
+    </div>
+  );
+}
